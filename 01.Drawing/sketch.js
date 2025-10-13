1eb36d93cf56132bc872b094dbd84804008b949c
nameenbutt <nameenbut20@gmail>
2025-10-13 15:38:52 +0100

    circle (-40,20,-30)

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -1,15 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
+createCanvas(400,400)
 }
 
 function draw() {
   background(220);
-  circle(250,250,250)
+  circle(250,250,250);
   fill(255,182,193)
   ellipse(250,250,250,250)
 
-  circle(40,20,30) 
-  createCanvas (400,400)
+  circle(-40,20,-30) 
   fill(40,20,30)
 
 
