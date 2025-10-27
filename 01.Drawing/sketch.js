c6441f5f4c93d41b263b458fe0047eef0697c13e
nameenbutt <nameenbut20@gmail>
2025-10-27 15:44:47 +0000

    15 and 22

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -14,14 +14,14 @@ function draw() {
   ellipse(200, 200, 100, 100);
    // LEFT pupil inside left eye
     fill(0);//black color
-    ellipse(285,200,20,20);//left pupil
+    ellipse(200,200,20,20);//left pupil centered
 
   //ellipse on the right side 
   fill(200,162,200);
     ellipse(300, 200, 100, 100);  // moved from (200, 200) to (300, 200)
     // RIGHT pupil inside right eye
     fill(0);//black color
-    ellipse(315,200,20,20);//right pupil
+    ellipse(300,200,20,20);//right pupil centered
 
 
     // --- Silly mouth ---
