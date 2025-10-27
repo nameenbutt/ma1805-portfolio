b43bd8c3068fa7c0a568026a5706bdfd9745e32b
nameenbutt <nameenbut20@gmail>
2025-10-27 15:11:37 +0000

    14

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -2,7 +2,7 @@ function setup() {
 createCanvas(400,400)
 }
 
-function draw() {
+function draw() {}
   background(220);
   circle(250,250,250);
   fill(255,182,193)
@@ -12,11 +12,4 @@ function draw() {
   ellipse(200, 200, 100, 100);
 
 //eyes
-  fill(0);
-  circle(50,50,50);
-  circle(50,50,50);
 
-  //mouth
-  fill(0,255,0,255)
-  ellipse(150,250,200,200);
-}
