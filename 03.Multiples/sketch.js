4bd8ff1e919f91efccf7bfa27c247f62805bcc18
nameenbutt <nameenbut20@gmail>
2025-10-31 21:06:25 +0000

    9-17

--- 03.Multiples/sketch.js
+++ 03.Multiples/sketch.js
@@ -10,7 +10,11 @@ background(0);
 let cols = 10;
 let rows = 8;
 
+//Loop through rows and columns
+for (let i = 0; i <cols; i++) {
+  for (let j = 0; j < rows; j++) {
 
-
-
+// calculate x and y position for each square
 }
+}
+ }
