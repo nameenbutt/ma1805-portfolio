621db0581793db2f3e153d981cc005c3a9295735
nameenbutt <nameenbut20@gmail>
2025-10-31 21:13:44 +0000

    13-35

--- 03.Multiples/sketch.js
+++ 03.Multiples/sketch.js
@@ -18,7 +18,18 @@ for (let i = 0; i <cols; i++) {
 let x = i * 60 + 30;
 let y = j * 50 + 25;
 
+// create color variation 
+let r = map (i, 0, cols, 50, 255);
+let g = map (j, 0, rows, 50, 255);
+let b = map (mouseX, 0, width, 100, 255); // reacts to mouse
 
+fill (r,g,b);
+rect (x,y, 50,50);
+
+//Title
+fill(255);
+textAlign(CENTER);
+text("Multiples: Color Grid - Move your mouse", width/2, height - 10);
 }
 }
  }
