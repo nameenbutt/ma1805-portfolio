6cc50e7590fac185186a39a6f6ea9cd979c74b92
nameenbutt <nameenbut20@gmail>
2025-10-31 20:44:28 +0000

    12-21

--- 03.Multiples/sketch.js
+++ 03.Multiples/sketch.js
@@ -3,18 +3,23 @@ function setup() {
   noStroke();
 }
 
-function draw() {
-  background(220);
-}
-
 let circles = []; 
 
 //Make 20 circles and store their info in an array
 for (let i = 0; i < 20; i++) 
   circles.push
 ({
-  
-})
+  x: i* 30+20 // position across the screen
+  , y: height / 2 // all start in the middle
+  , size: random (15, 30) // small varaition
+  , color: color(random(100,255), random (100,255), random (255)) 
+});
+
+
+function draw() {
+  background(20);
+}
+
 
 
 {
