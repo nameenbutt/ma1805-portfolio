cdc6051680869e8e8f9b48826f6e40c8898ef297
nameenbutt <nameenbut20@gmail>
2025-10-31 20:40:22 +0000

    1-16

--- 03.Multiples/sketch.js
+++ 03.Multiples/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 300);
+  noStroke();
 }
 
 function draw() {
   background(220);
+}
+
+let circles = []; 
+
+//Make 20 circles and store their info in an array
+for (let i = 0; i < 20; i++) 
+  circles.push
+({
+  
+})
+
+
+{
+
 }
